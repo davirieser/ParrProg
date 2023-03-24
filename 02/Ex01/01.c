613b5cebe3a81bdb521e92079dbe4eb1b8d6603a
Lukas Kirchmair <csaw9392@student.uibk.ac.at>
2023-03-24 09:23:58 +0100

    Corrected small error in mapping of cx.

--- 02/Ex01/01.c
+++ 02/Ex01/01.c
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,40 +7,42 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+// Macro functions
+#define SQR(x) ((x) * (x))
+
 // Default size of image
 #define X 1280
 #define Y 720
 #define MAX_ITER 10000
-
-#define MAX_BRIGHTNESS 255
-#define BAILOUT_VALUE (2 * 2)
+#define BAILOUT_VALUE SQR(2)
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-  for (uint i = 0; i < Y; i++) {
-    for (uint j = 0; j < X; j++) {
-      double cx = (((2 * (double)j) / X) - 1) * (X / Y),
-             cy = ((2 * (double)i) / Y) - 1;
-      uint iteration = 0;
-      double x = 0, y = 0;
+	for(uint32_t i_y = 0; i_y < Y; i_y++) {
+		for(uint32_t i_x = 0; i_x < X; i_x++) {
 
-      for (; (iteration < MAX_ITER) && ((x * x + y * y) <= BAILOUT_VALUE);
-           iteration++) {
-        double x_tmp = x * x - y * y + cx;
-        y = 2 * x * y + cy;
-        x = x_tmp;
-      }
+			double x = 0, y = 0;
+			double cx = i_x / (double)X * (1 + 2.5) - 2.5;
+			double cy = i_y / (double)Y * (1 + 1.0) - 1;
 
-      image[i][j] = (iteration * MAX_BRIGHTNESS) / MAX_ITER;
-    }
-  }
+			uint32_t iteration;
+			for(iteration = 0; iteration < MAX_ITER && SQR(x) + SQR(y) <= BAILOUT_VALUE;
+			    iteration++) {
+				double x_temp = SQR(x) - SQR(y) + cx;
+				y = 2 * x * y + cy;
+				x = x_temp;
+			}
+			double norm_iteration = iteration / (double)MAX_ITER * 255;
+			image[i_y][i_x] = norm_iteration;
+		}
+	}
 }
 
 int main() {
-  uint8_t image[Y][X];
+	uint8_t image[Y][X];
 
-  calc_mandelbrot(image);
+	calc_mandelbrot(image);
 
-  const int channel_nr = 1, stride_bytes = 0;
-  stbi_write_png("mandelbrot.png", X, Y, channel_nr, image, stride_bytes);
-  return EXIT_SUCCESS;
+	const int channel_nr = 1, stride_bytes = 0;
+	stbi_write_png("mandelbrot.png", X, Y, channel_nr, image, stride_bytes);
+	return EXIT_SUCCESS;
 }
