137a4ffd1c2c270b53dd536be613c8556200c141
Lukas Kirchmair <csaw9392@student.uibk.ac.at>
2023-05-13 11:39:58 +0200

    Configured toml to run for different problem size.

--- 07/Ex01/Vectorizing.c
+++ 07/Ex01/Vectorizing.c
@@ -3,24 +3,31 @@
 #include <sys/time.h>
 #include <omp.h>
 
-#define SIZE 1e5 // Size of the vectors
-#define REPETITIONS 1e4 // Number of repetitions
+#define ENVVAR_SIZE "ENVVAR_SIZE"
+#define REPETITIONS 1e6 // Number of repetitions
 
-void multiplyAdd(float* a, const float* b, const float* c) {
+void multiplyAdd(float* a, const float* b, const float* c, long size) {
     for (int run = 0; run < REPETITIONS; ++run) {
-        for (int i = 0; i < SIZE; ++i) {
+        for (int i = 0; i < size; ++i) {
             a[i] += b[i] * c[i];
         }
     }
 }
 
 int main() {
-    float* a = malloc(SIZE * sizeof(float));
-    float* b = malloc(SIZE * sizeof(float));
-    float* c = malloc(SIZE * sizeof(float));
+
+    char* endPtr;
+    const long size = strtol(getenv(ENVVAR_SIZE), &endPtr, 10);
+    if(*endPtr != '\0'){
+        printf("End pointer was not on \\0. Remaining: \"%s\"\n", endPtr);
+        return -1;
+    }
+    float* a = malloc(size * sizeof(float));
+    float* b = malloc(size * sizeof(float));
+    float* c = malloc(size * sizeof(float));
 
     // Initialize vectors with constant values
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < size; ++i) {
         a[i] = 0;
         b[i] = 1;
         c[i] = 2;
@@ -28,11 +35,11 @@ int main() {
 
     // Perform the computation and measure time
     double start = omp_get_wtime();
-    multiplyAdd(a, b, c);
+    multiplyAdd(a, b, c, size);
     double timeTaken = omp_get_wtime() - start;
 
     // Verify the result
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < size; ++i) {
         if (a[i] != (b[i] * c[i] * REPETITIONS)) {
             printf("Verification failed at index %d\n", i);
             break;
